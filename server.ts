7e879bda774b13e9621475317f210027eaa641dd
Nzanzu Lwanzo <nzanzu.lwanzo.work@gmail.com>
2025-12-12 05:07:10 +0200

    catch error in case of invalid coid param

--- server.ts
+++ server.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getFileContent } from './lib/helpers.ts';
+import { getFileContent, getISOCodes } from './lib/helpers.ts';
 import type { City, Country } from './lib/@types.ts';
 
 const app = express();
@@ -13,8 +13,17 @@ app.get('/countries', async (req, res) => {
 
 // Get cities
 app.get('/cities/:coid', async (req, res) => {
-  const cities = await getFileContent<City[]>(`data/${req.params.coid}.json`);
-  res.json(cities);
+  try {
+    const cities = await getFileContent<City[]>(
+      `data/${req.params.coid.toUpperCase()}.json`,
+    );
+    res.json(cities);
+  } catch {
+    res.json({
+      msg: 'Error, invalid coid parameter',
+      coids: await getISOCodes('data'),
+    });
+  }
 });
 
 app.listen(8888, () => console.log('Server is up and running'));
